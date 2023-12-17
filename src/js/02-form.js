ab345e53271060d858201a8644537b17f5134ab2
Maksum9 <103558917+Maksum9@users.noreply.github.com>
2023-12-17 10:26:49 +0200

    update

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,9 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const feedbackForm = document.querySelector('.feedback-form');
-    const savedState = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
-    feedbackForm.email.value = savedState.email || '';
-    feedbackForm.message.value = savedState.message || '';
-
+    
     feedbackForm.addEventListener('input', (event) => {
         const { name, value } = event.target;
         const currentState = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
@@ -12,10 +9,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     feedbackForm.addEventListener('submit', (event) => {
         event.preventDefault();
-        const currentState = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
-        localStorage.removeItem('feedback-form-state');
-        console.log({ email: currentState.email, message: currentState.message });
-        feedbackForm.email.value = '';
-        feedbackForm.message.value = '';
+
+        const emailInput = feedbackForm.querySelector('.input-name');
+        const messageInput = feedbackForm.querySelector('.textarea');
+
+        const valueEmail = emailInput.value.trim();
+        const valueMessage = messageInput.value.trim();
+
+        if (!valueEmail || !valueMessage) {
+            alert('Будь ласка, заповніть всі поля форми.');
+        } else {
+            const currentState = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+            localStorage.removeItem('feedback-form-state');
+            console.log({ email: currentState.email, message: currentState.message });
+            emailInput.value = '';
+            messageInput.value = '';
+            alert('Дані відправлені успішно!');
+        }
     });
+
+    const savedState = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+    feedbackForm.email.value = savedState.email || '';
+    feedbackForm.message.value = savedState.message || '';
 });
